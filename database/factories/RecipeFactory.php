d7ef9f58403f189195666bd14fa9d0aa58dd7f3d
Johan Camilo L.R. <johancalr11@gmail.com>
2025-10-07 08:01:58 -0500

    Create Factories

---
+++ database/factories/RecipeFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
+ */
+class RecipeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'title' => fake()->sentence(),
+            'category_id' => Category::all()->random()->id,
+            'description' => fake()->text(),
+            'ingredients' => fake()->text(),
+            'instructions' => fake()->text(),
+            'image' => fake()->imageUrl(640, 480, 'food', true),
+        ];
+    }
+}
